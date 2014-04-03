cd07ce15cb01be7ef8ca26c156da3a93d1cec7ac
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-04-03 18:48:40 +1100

    Added support for Hive style partitioning.

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/partition/Partition.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/partition/Partition.scala
@@ -20,3 +20,21 @@ object Partition {
       case List(y, m, d) => (y, m, d)
     }, "%s/%s/%s")
 }
+
+object HivePartition {
+  def byFields1[A, B](a: Field[A, B]): Partition[A, B] =
+    Partition(a.get, s"${a.value}=%s")
+
+  def byFields2[A, B, C](a: Field[A, B], b: Field[A, C]): Partition[A, (B, C)] =
+    Partition(v => (a.get(v), b.get(v)), s"${a.value}=%s/${b.value}=%s")
+
+  def byFields3[A, B, C, D](a: Field[A, B], b: Field[A, C], c: Field[A, D]): Partition[A, (B, C, D)] =
+    Partition(v => (a.get(v), b.get(v), c.get(v)), s"${a.value}=%s/${b.value}=%s/${c.value}=%s")
+
+  def byDate[A](date: Field[A, String]): Partition[A, (String, String, String)] =
+    Partition(v => date.get(v).split("-").toList match {
+      case List(y, m, d) => (y, m, d)
+    }, "year=%s/month=%s/day=%s")
+}
+
+
