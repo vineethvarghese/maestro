79ccd00e7fbc2619952f25ca985a37e3b6158547
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-05-01 16:00:16 +1000

    Reduced number of exceptions in decode macro.

--- maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/DecodeMacro.scala
+++ maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/DecodeMacro.scala
@@ -21,13 +21,10 @@ object DecodeMacro {
         if (vs.length < $size) {
           DecodeError(ValDecodeSource(vs), position, NotEnoughInput($size, $typeName))
         } else {
+          var index = 0
           val fields = vs.take($size).toArray
-          try {
-            val result = ${build(decodeVals(xs))}
-            DecodeOk((ValDecodeSource(vs.drop($size)), position + $size, result))
-          } catch {
-            case DecodeException(reason, index) => DecodeError(ValDecodeSource(vs.drop(index - position)), position + index, reason)
-          }
+          val result = ${build(decodeVals(xs))}
+          DecodeOk((ValDecodeSource(vs.drop($size)), position + $size, result))
         }
       }
     """
@@ -38,36 +35,62 @@ object DecodeMacro {
           DecodeError(UnknownDecodeSource(vs), position, NotEnoughInput($size, $typeName))
         } else {
           val fields = vs.take($size).toArray
+          var index = -1
+          var value = ""
+          var tag   = "" 
           try {
             val result = ${build(decodeUnknowns(xs))}
             DecodeOk((UnknownDecodeSource(vs.drop($size)), position + $size, result))
           } catch {
-            case DecodeException(reason, index) => DecodeError(UnknownDecodeSource(vs.drop(index - position)), position + index, reason)
+            case NonFatal(e) => DecodeError(
+              UnknownDecodeSource(vs.drop(index - position)),
+              position + index,
+              ParseError(fields(index), tag, That(e))
+            )
           }
         }
       }
     """
 
-    def build(args: List[c.universe.Apply]) = Apply(Select(Ident(companion), newTermName("apply")), args)
+    def build(args: List[Tree]) = Apply(Select(Ident(companion), newTermName("apply")), args)
 
-    def decodeVals(xs: List[(MethodSymbol, Int)]): List[c.universe.Apply] = xs.map { 
+    def decodeVals(xs: List[(MethodSymbol, Int)]): List[Match] = xs.map { 
       case (x, i) => {
-        val name = newTermName("as" + x.returnType + "Val")
-        q"""DecodeUtil.$name(fields(${i - 1}), ${i - 1})"""
+        val typeVal = newTermName(x.returnType + "Val")
+        val name    = x.returnType.toString
+        q"""
+          fields(${i - 1}) match {
+            case $typeVal(s) => {
+              index +=1
+              s
+            }
+            case x        =>
+              return DecodeError(ValDecodeSource(vs.drop(index - position)), position + index, ValTypeMismatch(x, $name))
+          }
+        """
       }
     }
 
-    def decodeUnknowns(xs: List[(MethodSymbol, Int)]): List[c.universe.Apply] = xs.map {
+    def decodeUnknowns(xs: List[(MethodSymbol, Int)]): List[Tree] = xs.map {
       case (x, i) if x.returnType == stringType => q"fields(${i - 1})"
       case (x, i)                               => {
-        val name = newTermName("as" + x.returnType + "Unknown")
-        q"""DecodeUtil.$name(fields(${i - 1}), ${i - 1})"""
+        val method = newTermName("to" + x.returnType)
+        val name    = x.returnType.toString
+        q"""{
+          tag    = $name
+          index  = ${i - 1}
+          fields(index).$method
+        }"""
       }
     }
 
+    //
+
     val combined = q"""Decode((source, position) => {
-      import au.com.cba.omnia.maestro.core.data.Val
-      import au.com.cba.omnia.maestro.core.codec.{DecodeSource, ValDecodeSource, UnknownDecodeSource, DecodeOk, DecodeError, DecodeResult, ParseError, ValTypeMismatch, NotEnoughInput, DecodeUtil}
+      import scala.util.control.NonFatal
+      import scalaz.\&/.That
+      import au.com.cba.omnia.maestro.core.data.{Val, BooleanVal, IntVal, LongVal, DoubleVal, StringVal}
+      import au.com.cba.omnia.maestro.core.codec.{DecodeSource, ValDecodeSource, UnknownDecodeSource, DecodeOk, DecodeError, DecodeResult, ParseError, ValTypeMismatch, NotEnoughInput}
 
       ${decodeValSource(members)}
       ${decodeUnknownSource(members)}
--- maestro-macros/src/test/scala/au/com/cba/omnia/maestro/macros/DecodeMacroSpec.scala
+++ maestro-macros/src/test/scala/au/com/cba/omnia/maestro/macros/DecodeMacroSpec.scala
@@ -1,6 +1,7 @@
 package au.com.cba.omnia.maestro.macros
 
 import au.com.cba.omnia.maestro.core.codec._
+import au.com.cba.omnia.maestro.core.data._
 import au.com.cba.omnia.maestro.macros._
 
 import au.com.cba.omnia.maestro.test.Spec
@@ -12,15 +13,20 @@ object DecodeMacroSpec extends Spec { def is = s2"""
 DecodeMacro
 ===========
 
-  decode from UnknownDecodeSource $unknown
-  decode from ValDecodeSource     $valdecode
+  decode from UnknownDecodeSource       $unknown
+  decode from ValDecodeSource           $valdecode
+  type mismatch ValDecodeSource         $typeErrorVal
+  type mismatch UnknownDecodeSource     $typeErrorUnknown
+  not enough fields ValDecodeSource     $sizeErrorVal
+  not enough fields UnknownDecodeSource $sizeErrorUnknown
 
 """
 
   implicit val encode = Macros.mkEncode[Customer]
+  implicit val decode = Macros.mkDecode[Customer]
 
   def valdecode = prop { (c: Customer) =>
-    Macros.mkDecode[Customer].decode(ValDecodeSource(Encode.encode(c))) must_== DecodeOk(c)
+    decode.decode(ValDecodeSource(Encode.encode(c))) must_== DecodeOk(c)
   }
 
   def unknown = prop { (c: Customer) =>
@@ -29,6 +35,51 @@ DecodeMacro
       c.customerSubCat, c.customerBalance.toString, c.effectiveDate
     ))
 
-    Macros.mkDecode[Customer].decode(unknown) must_== DecodeOk(c)
+    decode.decode(unknown) must_== DecodeOk(c)
   }
+
+  def typeErrorVal = {
+    val encoded = ValDecodeSource(List(
+      StringVal("1"), StringVal("2"), StringVal("3"), StringVal("4"), StringVal("5"),
+      StringVal("6"), StringVal("7")
+    ))
+    decode.decode(encoded) must_== DecodeError(
+      ValDecodeSource(List(StringVal("6"), StringVal("7"))),
+      5,
+      ValTypeMismatch(StringVal("6"), "Int")
+    )
+  }
+
+  def sizeErrorVal = {
+    decode.decode(ValDecodeSource(List())) must_== DecodeError(
+      ValDecodeSource(List()),
+      0,
+      NotEnoughInput(7, "au.com.cba.omnia.maestro.test.thrift.Customer")
+    )
+  }
+
+  def typeErrorUnknown = {
+    val encoded = UnknownDecodeSource(List(
+      "1", "2", "3", "4", "5", "not an int", "7"
+    ))
+    decode.decode(encoded) must parseErrorMatcher[Customer](DecodeError(
+      UnknownDecodeSource(List("not an int", "7")),
+      5,
+      ParseError("not an int", "Int", null)
+    ))
+  }
+
+  def sizeErrorUnknown = {
+    decode.decode(UnknownDecodeSource(List())) must_== DecodeError(
+      UnknownDecodeSource(List()),
+      0,
+      NotEnoughInput(7, "au.com.cba.omnia.maestro.test.thrift.Customer")
+    )
+  }
+
+  def parseErrorMatcher[T] = (be_==(_: DecodeResult[T])) ^^^ ((_: DecodeResult[T]) match {
+    case e@DecodeError(_, _, r@ParseError(_, _, _)) =>
+      e.copy(reason = r.copy(error = null))
+    case other => other
+  })
 }
