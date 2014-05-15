d5973c3020a93aac5fc7a246d8284de50adc38ba
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-05-15 12:19:13 +1000

    Added unsafe non paramaterised versions of the Inspect methods so that they can be used in macros dynamically.
    Also tightened up how Inspect finds the fields in ThriftStructs.

--- maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/Inspect.scala
+++ maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/Inspect.scala
@@ -11,35 +11,40 @@ import au.com.cba.omnia.humbug.HumbugThriftStruct
 object Inspect {
   val ProductField = """_(\d+)""".r
 
-  def indexed[A <: ThriftStruct: c.WeakTypeTag](c: Context): List[(c.universe.MethodSymbol, Int)] = {
+  def indexed[A <: ThriftStruct: c.WeakTypeTag](c: Context): List[(c.universe.MethodSymbol, Int)] = 
+    indexedUnsafe(c)(c.universe.weakTypeOf[A])
+
+  def indexedUnsafe(c: Context)(typ: c.universe.Type): List[(c.universe.MethodSymbol, Int)] = {
     import c.universe._
-    c.universe.weakTypeOf[A].members.toList.map(member => (member, member.name.toString)).collect({
+    typ.members.toList.map(member => (member, member.name.toString)).collect({
       case (member, ProductField(n)) =>
         (member.asMethod, n.toInt)
     }).sortBy(_._2)
   }
 
-  def fields[A <: ThriftStruct: c.WeakTypeTag](c: Context): List[(c.universe.MethodSymbol, String)] = {
+  def fields[A <: ThriftStruct: c.WeakTypeTag](c: Context): List[(c.universe.MethodSymbol, String)] =
+    fieldsUnsafe(c)(c.universe.weakTypeOf[A])
+
+
+  def fieldsUnsafe(c: Context)(typ: c.universe.Type): List[(c.universe.MethodSymbol, String)] = {
     import c.universe._
 
     val fields =
-      if (c.universe.weakTypeOf[A] <:< c.universe.weakTypeOf[HumbugThriftStruct])
-        c.universe.weakTypeOf[A].members.toList
-          .map(_.name.toString)
-          .filter(x => x.endsWith("_$eq") && !x.startsWith("_"))
-          .map(_.replaceAll("_\\$eq$", "").capitalize)
-          .reverse
+      if (typ <:< c.universe.weakTypeOf[HumbugThriftStruct])
+        typ.declarations.toList.collect {
+          case sym: TermSymbol if sym.isVar => sym.name.toString.trim.capitalize
+        }
       else
-        c.universe.weakTypeOf[A].typeSymbol.companionSymbol.typeSignature.members.toList
-          .map(_.name.toString)
-          .filter(x =>
-            x.endsWith("Field") && !x.startsWith("write")
-          ).map(_.replaceAll("Field$", ""))
-          .reverse
+        typ.typeSymbol.companionSymbol.typeSignature
+          .member(newTermName("apply")).asMethod.paramss.head.map(_.name.toString.capitalize)
 
-    methods(c).zip(fields)
+    methodsUnsafe(c)(typ).zip(fields)
   }
 
   def methods[A <: ThriftStruct: c.WeakTypeTag](c: Context): List[c.universe.MethodSymbol] =
     indexed(c).map({ case (method, _) => method })
+
+
+  def methodsUnsafe(c: Context)(typ: c.universe.Type): List[c.universe.MethodSymbol] =
+    indexedUnsafe(c)(typ).map({ case (method, _) => method })
 }
