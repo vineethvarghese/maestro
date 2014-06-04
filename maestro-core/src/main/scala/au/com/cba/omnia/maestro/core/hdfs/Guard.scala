544474d7e1bb3de4f21b5f5b7630bb5df18ea28e
Stephan Hoermann <stephan.hoermann@cba.com.au>
2014-06-04 13:13:03 +1000

    Changed Guard.expandPath to accept custom filters.
    Added filters for not processed directories and for ingestion
    complete. This closes #61.

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/hdfs/Guard.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/hdfs/Guard.scala
@@ -4,14 +4,28 @@ package hdfs
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
+case class GuardFilter(filter: (FileSystem, Path) => Boolean) {
+  def &&&(that: GuardFilter): GuardFilter =
+    GuardFilter((fs, p) => filter(fs, p) && that.filter(fs, p))
+}
+
 object Guard {
-  def expandPaths(path: String): List[String] = {
+  /** Filter out any directories that HAVE a _PROCESSED file. */
+  val NotProcessed = GuardFilter((fs, p) => !fs.exists(new Path(p, "_PROCESSED")))
+  /** Filter out any directories that DO NOT HAVE a _INGESTION_COMPLETE file. */
+  val IngestionComplete = GuardFilter((fs, p) => fs.exists(new Path(p, "_INGESTION_COMPLETE")))
+
+  /** Expands the globs in the provided path and only keeps those directories that pass the filter. */
+  def expandPaths(path: String, filter: GuardFilter = NotProcessed): List[String] = {
     val fs = FileSystem.get(new Configuration)
     fs.globStatus(new Path(path))
       .toList
       .filter(s => fs.isDirectory(s.getPath))
       .map(_.getPath)
-      .filter(p => !fs.exists(new Path(p, "_PROCESSED")))
+      .filter(filter.filter(fs, _))
       .map(_.toString)
   }
+
+  /** As `expandPath` but the filter is `NotProcessed` and `IngestionComplete`. */
+  def expandTransferredPaths(path: String) = expandPaths(path, NotProcessed &&& IngestionComplete)
 }
