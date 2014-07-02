913b97b57c9c04111e00fcb7a3d8c7734b901988
Stephan Hoermann <Stephan Hoermann>
2014-07-03 09:22:07 +1000

    Added code to query hive tables.

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/task/Query.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/task/Query.scala
@@ -1,18 +1,49 @@
-package au.com.cba.omnia.maestro.core
-package task
+//   Copyright 2014 Commonwealth Bank of Australia
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
-import hive._
-import scalding._
-import codec._
-import au.com.cba.omnia.ebenezer.scrooge.hive._
-import com.twitter.scalding._
+package au.com.cba.omnia.maestro.core.task
+
 import org.apache.hadoop.hive.conf.HiveConf
-import au.com.cba.omnia.ebenezer.scrooge.hive.HiveJob
-import cascading.flow.FlowDef
-import partition._
-import com.twitter.scrooge.ThriftStruct
-import com.twitter.scalding.typed.EmptyTypedPipe
 
+import com.twitter.scalding.{Args, Job}
+
+import au.com.cba.omnia.ebenezer.scrooge.hive.HiveJob
+
+import au.com.cba.omnia.maestro.core.hive.HiveTable
+
+/** A trait for API to run hive queries.*/
 trait Query {
-  // hqlQuery goes here
+  /** 
+    * Runs the specified hive query.
+    * 
+    * The input and output tables are used to determine dependencies for scheduling.
+    */
+  def hiveQuery(
+    args: Args, name: String, query: String,
+    inputs: List[HiveTable[_, _]], output: Option[HiveTable[_, _]], conf: HiveConf
+  ) : Job =
+    HiveJob(args, name, query, inputs.map(_.source(conf)), output.map(_.sink(conf)))
+
+  /**
+    * Runs the specified hive query.
+    * 
+    * The input and output tables are used to determine dependencies for scheduling.
+    */
+  def hiveQuery(
+    args: Args, name: String, query: String,
+    input: HiveTable[_, _], output: Option[HiveTable[_, _]], conf: HiveConf
+  ) : Job =
+    HiveJob(args, name, query, input.source(conf), output.map(_.sink(conf)))
+
 }
