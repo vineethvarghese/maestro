ef368aaf31d7b25989b61c7e966c9317d7c58b0f
Stephan Hoermann <Stephan Hoermann>
2014-07-03 09:21:49 +1000

    Added hiveView to view to write out to a hive table.

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/task/View.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/task/View.scala
@@ -12,8 +12,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-package au.com.cba.omnia.maestro.core
-package task
+package au.com.cba.omnia.maestro.core.task
 
 import cascading.flow.FlowDef
 
@@ -21,18 +20,35 @@ import com.twitter.scalding._, TDsl._
 
 import com.twitter.scrooge.ThriftStruct
 
+import org.apache.hadoop.hive.conf.HiveConf
+
 import au.com.cba.omnia.ebenezer.scrooge.PartitionParquetScroogeSource
 
 import au.com.cba.omnia.maestro.core.partition.Partition
+import au.com.cba.omnia.maestro.core.hive.HiveTable
 
 trait View {
   /** Partitions a pipe using the given partition scheme and writes out the data.*/
-  def view[A <: ThriftStruct : Manifest, B: Manifest: TupleSetter]
+  def view[A <: ThriftStruct : Manifest, B : Manifest : TupleSetter]
     (partition: Partition[A, B], output: String)
     (pipe: TypedPipe[A])
     (implicit flowDef: FlowDef, mode: Mode): Unit = {
     pipe
       .map(v => partition.extract(v) -> v)
       .write(PartitionParquetScroogeSource[B, A](partition.pattern, output))
+  }
+
+  /**
+    * Partitions a pipe using the given partition scheme and writes out the data to a hive table.
+    * 
+    * This will create the table if it doesn't already exist. If the existing schema doesn't match
+    * the schema expected the job will fail.
+    */
+  def viewHive[A <: ThriftStruct : Manifest, B : Manifest : TupleSetter]
+    (table: HiveTable[A, B], conf: HiveConf) (pipe: TypedPipe[A])
+    (implicit flowDef: FlowDef, mode: Mode): Unit = {
+    pipe
+      .map(v => table.partition.extract(v) -> v)
+      .write(table.sink(conf))
   }
 }
