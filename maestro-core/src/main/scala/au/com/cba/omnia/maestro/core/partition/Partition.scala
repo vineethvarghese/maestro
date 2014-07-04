53b1d6889c32701d5812d585f58dccff3035874a
Stephan Hoermann <Stephan Hoermann>
2014-07-04 11:03:13 +1000

    Added documentation for Partition.

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/partition/Partition.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/partition/Partition.scala
@@ -17,15 +17,26 @@ package partition
 
 import au.com.cba.omnia.maestro.core.data._
 
+/**
+  * Partitioning schema to create partition `B`, usually a string or tuple of strings
+  * from `A`.
+  * 
+  * It has the names of the partition columns, a way to get the partition values from an instance
+  * of `A` and a pattern to create from the values, e.g `"id=%s"`.
+  */
 case class Partition[A, B](fieldNames: List[String], extract: A => B, pattern: String)
 
+/** Various partitioning schemes. */
 object Partition {
+  /** Partitions by the specified field.*/
   def byField[A, B](a: Field[A, B]): Partition[A, B] =
     Partition(toPartitionName(List(a.name)), a.get, "%s")
 
+  /** Partitions by the two specified fields.*/
   def byFields2[A, B, C](a: Field[A, B], b: Field[A, C]): Partition[A, (B, C)] =
     Partition(toPartitionName(List(a.name, b.name)), v => (a.get(v), b.get(v)), "%s/%s")
 
+  /** Partitions by the three specified fields.*/
   def byFields3[A, B, C, D](a: Field[A, B], b: Field[A, C], c: Field[A, D]): Partition[A, (B, C, D)] =
     Partition(
       toPartitionName(List(a.name, b.name, c.name)),
@@ -33,11 +44,19 @@ object Partition {
       "%s/%s/%s"
     )
 
+  /**
+    * Partitions by year, month, day. Expects to the specified field to have data in the
+    * format `yyyy-mm-dd`.
+    */
   def byDate[A](date: Field[A, String]): Partition[A, (String, String, String)] =
     Partition(List("year", "month", "day"), v => date.get(v).split("-").toList match {
       case List(y, m, d) => (y, m, d)
     }, "%s/%s/%s")
 
+  /**
+    * Partitions by year, month, day, hour. Expects to the specified field to have data in the
+    * format `yyyy-mm-dd-hh`.
+    */
   def byHour[A](date: Field[A, String]): Partition[A, (String, String, String, String)] =
     Partition(List("year", "month", "day", "hour"), v => date.get(v).split("-").toList match {
       case List(y, m, d, h) => (y, m, d, h)
@@ -50,10 +69,16 @@ object Partition {
   def toPartitionName(columns: List[String]): List[String] = columns.map("partition_" + _)
 }
 
+/** 
+  * Various partitioning schemes using a hive style output path such as
+  * `field_name=value`.
+  */
 object HivePartition {
+  /** Hive style partition by the specified field.*/
   def byField[A, B](a: Field[A, B]): Partition[A, B] =
     Partition(Partition.toPartitionName(List(a.name)), a.get, s"p${a.name}=%s")
 
+  /** Hive style partition by the two specified fields.*/
   def byFields2[A, B, C](a: Field[A, B], b: Field[A, C]): Partition[A, (B, C)] =
     Partition(
       Partition.toPartitionName(List(a.name, b.name)),
@@ -61,6 +86,7 @@ object HivePartition {
       s"p${a.name}=%s/p${b.name}=%s"
     )
 
+  /** Hive style partition by the three specified fields.*/
   def byFields3[A, B, C, D](a: Field[A, B], b: Field[A, C], c: Field[A, D]): Partition[A, (B, C, D)] =
     Partition(
       Partition.toPartitionName(List(a.name, b.name, c.name)),
@@ -68,11 +94,19 @@ object HivePartition {
       s"p${a.name}=%s/p${b.name}=%s/p${c.name}=%s"
     )
 
+  /**
+    * Hive style partition by year, month, day. Expects to the specified field to have data in the
+    * format `yyyy-mm-dd`.
+    */
   def byDate[A](date: Field[A, String]): Partition[A, (String, String, String)] =
     Partition(List("year", "month", "day"), v => date.get(v).split("-").toList match {
       case List(y, m, d) => (y, m, d)
     }, "year=%s/month=%s/day=%s")
 
+  /**
+    * Hive style partition by year, month, day, hour. Expects to the specified field to have data in
+    * the format `yyyy-mm-dd-hh`.
+    */
   def byHour[A](date: Field[A, String]): Partition[A, (String, String, String, String)] =
     Partition(List("year", "month", "day", "hour"), v => date.get(v).split("-").toList match {
       case List(y, m, d, h) => (y, m, d, h)
