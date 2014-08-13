5b529693a153530d85d0fc0b596a5865eac2b65a
Stephan Hoermann <srhoermann@gmail.com>
2014-08-13 10:17:25 +1000

    Fixes bugs in delimitedThermomterRecordReader for tests.
    Changes it to use the google splitter and get it to fail if it can't
    parse a line.

--- maestro-test/src/main/scala/au/com/cba/omnia/maestro/test/Records.scala
+++ maestro-test/src/main/scala/au/com/cba/omnia/maestro/test/Records.scala
@@ -1,11 +1,15 @@
 package au.com.cba.omnia.maestro.test
 
+import scala.collection.JavaConverters._
+
 import scalaz.effect.IO
 
-import au.com.cba.omnia.maestro.core.codec._
+import com.google.common.base.Splitter
 
 import au.com.cba.omnia.thermometer.context.Context
 import au.com.cba.omnia.thermometer.core.ThermometerRecordReader
+
+import au.com.cba.omnia.maestro.core.codec._
 
 trait Records {
   /**
@@ -14,10 +18,12 @@ trait Records {
    **/
   def delimitedThermometerRecordReader[A](delimiter: Char, decoder: Decode[A]) =
     ThermometerRecordReader((conf, path) => IO {
+      val splitter = Splitter.on(delimiter)
+
       new Context(conf).lines(path).map(l =>
-        decoder.decode(UnknownDecodeSource(l.split(delimiter).toList)) match {
-          case DecodeOk(c) => Some(c)
-          case _           => None
-        }).flatten
+        decoder.decode(UnknownDecodeSource(splitter.split(l).asScala.toList)) match {
+          case DecodeOk(c)       => c
+          case e: DecodeError[A] => throw new Exception(s"Can't decode line $l in $path: $e")
+        })
     })
 }
