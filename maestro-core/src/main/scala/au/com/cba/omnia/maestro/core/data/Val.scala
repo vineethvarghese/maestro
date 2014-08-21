593aa8c741ea63cef9813f21ddd98b6d4fe1d785
Stephan Hoermann <srhoermann@gmail.com>
2014-08-21 14:57:19 +1000

    Adds an OptionVal.

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/data/Val.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/data/Val.scala
@@ -20,11 +20,13 @@ import scalaz._, Scalaz._
 /* ☠ NOTE: this is called Val to avoid akward name clash with call by Value, please do not change ... ☠ */
 
 sealed trait Val
+
 case class BooleanVal(v: Boolean) extends Val
 case class IntVal(v: Int) extends Val
 case class LongVal(v: Long) extends Val
 case class DoubleVal(v: Double) extends Val
 case class StringVal(v: String) extends Val
+case object NoneVal extends Val
 
 object Val {
   implicit def ValEqual: Equal[Val] =
