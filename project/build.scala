5edca21e7c4bb325c6b62c74d6e754171ea55a77
Ben Lippmeier <Ben.Lippmeier@cba.com.au>
2014-08-27 18:01:04 +1000

    Add schemas to build file, and re-instate assembly command

--- project/build.scala
+++ project/build.scala
@@ -50,7 +50,7 @@ object build extends Build {
          publishArtifact := false
        , addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full)
     )
-  , aggregate = Seq(core, macros, api, test, schema)
+  , aggregate = Seq(core, macros, api, test, example, schema)
   )
 
   lazy val api = Project(
@@ -119,14 +119,11 @@ object build extends Build {
        standardSettings
     ++ uniform.project("maestro-schema", "au.com.cba.omnia.maestro.schema")
     ++ Seq[Sett](
-      sourceGenerators in Compile <+= sourceManaged in Compile map { outDir: File =>
-        GenUnravelPipes.gen(outDir)
-      },
-      libraryDependencies ++= Seq (
-        "com.quantifind"  %% "sumac"  % "0.3.0"
-      ) ++ depend.scalding() ++ depend.hadoop()
+          libraryDependencies ++= Seq (
+            "com.quantifind"  %% "sumac"  % "0.3.0"
+          ) ++ depend.scalding() ++ depend.hadoop()
+       )
     )
-  )
 
   lazy val example = Project(
     id = "example"
