66ab77e7ef40cd28fc8d8e1c8ca3ebf1a9491ae9
Tim Sell <trsell@gmail.com>
2014-09-09 17:53:31 +1000

    javadoc, cleanup, simplify

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/task/Sqoop.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/task/Sqoop.scala
@@ -1,35 +1,96 @@
 package au.com.cba.omnia.maestro.core.task
 
 import java.io.File
-import com.cloudera.sqoop.SqoopOptions
-import com.twitter.scalding.{ Args, Job, Mode }
-import au.com.cba.omnia.parlour.{ ExportSqoopJob, ImportSqoopJob }
-import cascading.flow.FlowDef
-import au.com.cba.omnia.parlour.ParlourImportOptions
-import au.com.cba.omnia.parlour.ParlourOptions
-import au.com.cba.omnia.parlour.SqoopSyntax.ParlourImportDsl
 
+import com.twitter.scalding.{Args, Job, Mode}
+
+import org.apache.log4j.Logger
+import org.joda.time.{DateTime, DateTimeZone}
+
+import au.com.cba.omnia.parlour.{ExportSqoopJob, ImportSqoopJob, ParlourExportOptions, ParlourImportOptions}
+import au.com.cba.omnia.parlour.SqoopSyntax.{ParlourExportDsl, ParlourImportDsl}
+
+import cascading.flow.FlowDef
+
+/**
+  * Import and export data between a database and HDFS.
+  * 
+  * Methods return a list of jobs which can be added to a cascade.
+  * 
+  * See the example at `au.com.cba.omnia.maestro.example.CustomerSqoopExample`.
+  * 
+  * For a given `domain` and `tableName`, [[sqoopImport]] imports data files 
+  * to the standard HDFS location: `\$hdfsRoot/source/\$source/\$domain/\$tableName`.
+  *
+  * Only use [[customSqoopImport]] if we are required to use non-standard target location.
+  * 
+  * [[sqoopExport]] assumes export dir data is in PSV format.
+  */
 trait Sqoop {
-  def sqoopImport(
+  /**
+   * Run a sqoop import using custom import options
+   * 
+   * @param options: Custom import options
+   */
+  def customSqoopImport(
     options: ParlourImportOptions[_])(args: Args
-  )(implicit flowDef: FlowDef, mode: Mode): (Job, List[String]) = {
+  )(implicit flowDef: FlowDef, mode: Mode): (List[Job], List[String]) = {
+    val logger = Logger.getLogger("Sqoop")
     val sqoopOptions = options.toSqoopOptions
-    (new ImportSqoopJob(sqoopOptions)(args), List(sqoopOptions.getTargetDir()))
+    logger.info("Start of sqoop import")
+    logger.info(s"connectionString = ${sqoopOptions.getConnectString}")
+    logger.info(s"tableName        = ${sqoopOptions.getTableName}")
+    logger.info(s"targetDir        = ${sqoopOptions.getTargetDir}")
+    val job = new ImportSqoopJob(sqoopOptions)(args)
+    (List(job), List(sqoopOptions.getTargetDir))
   }
 
-  def sqoopImport(
+  /**
+    * Runs a sqoop import from a database to HDFS.
+    *
+    * Data will be copied onto HDFS at the following directory:
+    * `\$hdfsRoot/source/\$source/\$domain/\$tableName/<year>/<month>/<day>/`.
+    *
+    * Year, month and day are derived from the current date.
+    * 
+    * @param hdfsRoot: Root directory of HDFS
+    * @param source: Source system
+    * @param domain: Database or project within source
+    * @param tableName: Table name or file name in database or project
+    * @param options: Extra import options
+    * @return Tuple of list of jobs for this import and list of imported directories.
+    */
+  def sqoopImport[T <: ParlourImportOptions[T]](
     hdfsRoot: String,
     source: String,
     domain: String,
     tableName: String,
-    options: ParlourImportOptions[_]
-  )(args: Args)(implicit flowDef: FlowDef, mode: Mode): (Job, List[String]) = {
-
-    val hdfsLandingDir = List(hdfsRoot, "source", source, domain, tableName) mkString File.separator
+    options: ParlourImportOptions[T] = ParlourImportDsl()
+  )(args: Args)(implicit flowDef: FlowDef, mode: Mode): (List[Job], List[String]) = {
+    val date = DateTime.now(DateTimeZone.UTC).toString(List("yyyy","MM", "dd") mkString File.separator)
+    val hdfsLandingDir = List(hdfsRoot, "source", source, domain, tableName, date) mkString File.separator
     options.targetDir(hdfsLandingDir)
-    (new ImportSqoopJob(options.toSqoopOptions)(args), List(hdfsLandingDir))
+    customSqoopImport(options)(args)
   }
 
+  /**
+    * Runs a sqoop import from a database to HDFS.
+    *
+    * Data will be copied into HDFS at the following directory:
+    * `\$hdfsRoot/source/\$source/\$domain/\$tableName/<year>/<month>/<day>/`.
+    *
+    * Year, month and day are derived from the current date.
+    * 
+    * @param hdfsRoot: Root directory of HDFS
+    * @param source: Source system
+    * @param domain: Database or project within source
+    * @param tableName: Table name or file name in database or project
+    * @param connectionString: Jdbc url for connecting to the database
+    * @param username: Username for connecting to the database
+    * @param password: Password for connecting to the database
+    * @param options: Extra import options 
+    * @return Tuple of list of jobs for this import and list of imported directories
+    */
   def sqoopImport[T <: ParlourImportOptions[T]](
     hdfsRoot: String,
     source: String,
@@ -38,8 +99,8 @@ trait Sqoop {
     connectionString: String,
     username: String,
     password: String,
-    options: ParlourImportOptions[T] = new ParlourImportDsl()
-  )(args: Args)(implicit flowDef: FlowDef, mode: Mode): (Job, List[String]) = {
+    options: ParlourImportOptions[T] = ParlourImportDsl()
+  )(args: Args)(implicit flowDef: FlowDef, mode: Mode): (List[Job], List[String]) = {
     options.connectionString(connectionString)
       .username(username)
       .password(password)
@@ -47,7 +108,42 @@ trait Sqoop {
     sqoopImport(hdfsRoot, source, domain, tableName, options)(args)
   }
 
-  def sqoopExport(options: SqoopOptions)(args: Args)(implicit flowDef: FlowDef, mode: Mode): Job = {
-    new ExportSqoopJob(options)(args)
+  /**
+    * Runs a sqoop export from HDFS to a database.
+    *
+    * @param options: Custom export options 
+    * @return List of jobs for this export
+    */
+  def customSqoopExport[T <: ParlourExportOptions[T]](
+    options: ParlourExportOptions[T]
+  )(args: Args)(implicit flowDef: FlowDef, mode: Mode): List[Job] = {
+    List(new ExportSqoopJob(options.toSqoopOptions)(args))
+  }
+
+  /**
+    * Runs a sqoop export from HDFS to a database.
+    *
+    * @param exportDir: Directory containing data to be exported
+    * @param tableName: Table name or file name in database or project
+    * @param connectionString: Jdbc url for connecting to the database
+    * @param username: Username for connecting to the database
+    * @param password: Password for connecting to the database
+    * @param options: Extra export options 
+    * @return List of jobs for this export
+    */
+  def sqoopExport[T <: ParlourExportOptions[T]](
+    exportDir: String,
+    tableName: String,
+    connectionString: String,
+    username: String,
+    password: String,
+    options: ParlourExportOptions[T] = ParlourExportDsl()
+  )(args: Args)(implicit flowDef: FlowDef, mode: Mode): List[Job] = {
+    options.exportDir(exportDir)
+      .tableName(tableName)
+      .connectionString(connectionString)
+      .username(username)
+      .password(password)
+    customSqoopExport(options)(args)
   }
 }
--- maestro-example/src/main/scala/au/com/cba/omnia/maestro/example/CustomerSqoopCascade.scala
+++ maestro-example/src/main/scala/au/com/cba/omnia/maestro/example/CustomerSqoopCascade.scala
@@ -17,6 +17,8 @@ package au.com.cba.omnia.maestro.example
 
 import com.twitter.scalding.Args
 
+import scalaz.{Tag => _, _}, Scalaz._
+
 import au.com.cba.omnia.maestro.api.{MaestroCascade, Partition, RowFilter, UniqueJob, Validator}
 import au.com.cba.omnia.maestro.api.Clean
 import au.com.cba.omnia.maestro.api.Maestro.{load, now, view}
@@ -25,16 +27,19 @@ import au.com.cba.omnia.maestro.example.thrift.Customer
 import au.com.cba.omnia.parlour.SplitByAmp
 import au.com.cba.omnia.parlour.SqoopSyntax.TeradataParlourImportDsl
 
+
 class CustomerSqoopCascade(args: Args) extends MaestroCascade[Customer](args) with Sqoop {
   val hdfsRoot = args("hdfs-root")
   val source = "teradata"
   val domain = "marketing"
   val tablename = "customer"
-  val sqoopMappers = args("sqoop-mappers").toInt
-  val dbHost = args("host")
-  val dbDatabase = domain
-  val dbUsername = args("username")
-  val dbPassword = args("password")
+  val mappers = args("mappers").toInt
+  val host = args("host")
+  val database = domain
+  val connectionString = s"jdbc:teradata://${host}/MODE=ANSI,CHARSET=UTF8,DATABASE=${database}"
+  val username = args("username")
+  val password = args("password")
+
   val errors = s"${hdfsRoot}/errors/${domain}"
   val filter = RowFilter.keep
   val cleaners = Clean.all(
@@ -44,25 +49,20 @@ class CustomerSqoopCascade(args: Args) extends MaestroCascade[Customer](args) wi
   val customerView = s"${hdfsRoot}/view/warehouse/${domain}/${tablename}"
 
   /**
-   * In order for sqoop to work, you will need to include the teradata drivers and cloudera connector 
+   * In order for sqoop to work with Teradata, you will need to include the teradata drivers and cloudera connector 
    * in the maestro-example/lib folder when building the assembly. 
    */
-  /*val importOptions = TeradataParlourImportDsl()
-    .connectionString(s"jdbc:teradata://${dbHost}/MODE=ANSI,CHARSET=UTF8,DATABASE=${dbDatabase}")
-    .username(dbUsername)
-    .password(dbPassword)
-    .numberOfMappers(sqoopMappers)
+   val importOptions = TeradataParlourImportDsl()
+    .numberOfMappers(mappers)
     .inputMethod(SplitByAmp)
     .tableName(tablename)
-    */
-  val importOptions = TeradataParlourImportDsl()
-  val (sqoopJob, imported) = sqoopImport(" ", " ", " ", hdfsRoot, source, domain, tablename, importOptions)(args)
 
-  val jobs = Seq(
-    sqoopJob,
+  val (sqoopJobs, imported) = sqoopImport(hdfsRoot, source, domain, tablename, connectionString, username, password, importOptions)(args)
+
+  val jobs = sqoopJobs ++ Seq(
     new UniqueJob(args) {
-      val in  = load[Customer]("|", imported, errors, now(), cleaners, validators, filter, "null")
-      val out = view(Partition.byField(Fields.Cat), customerView)(in)
+      load[Customer]("|", imported, errors, now(), cleaners, validators, filter, "null") |>
+      view(Partition.byField(Fields.Cat), customerView)
     }
   )
 }
